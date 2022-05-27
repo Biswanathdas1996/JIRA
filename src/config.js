78b70aadfe86e3a605ff36584a59c82047a67c1c
your_username <your_email_address@example.com>
2022-05-27 06:58:20 +0530

    ok

--- src/config.js
+++ src/config.js
@@ -6,15 +6,12 @@ export const EtherscanAPIKEY = `WCVDU52748WW4F7EKDEDB89HKH41BIA4N2`;
 
 export const IPFSLink = `https://ipfs.infura.io:5001/api/v0`;
 
-// export const WalletPrivateKey =
-//   "33e8389993eea0488d813b34ee8d8d84f74f204c17b95896e9380afc6a514dc7";
 export const WalletPrivateKey =
-  "82e4fb5555837b975e4402a02c2fbe230ae7d4d61574ee00ed2b1ff79be84195";
+  "33e8389993eea0488d813b34ee8d8d84f74f204c17b95896e9380afc6a514dc7";
 
 export const InfuraProjectId = `24022fda545f41beb59334bdbaf3ef32`;
 
-// export const InfuraNodeURL = `https://${Network}.infura.io/v3/${InfuraProjectId}`;
-export const InfuraNodeURL = `http://192.168.0.106:7545/`;
+export const InfuraNodeURL = `https://${Network}.infura.io/v3/${InfuraProjectId}`;
 
 export const IpfsViewLink = (fingerprint) =>
   `https://ipfs.infura.io/ipfs/${fingerprint}`;
